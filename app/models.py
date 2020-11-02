5668df5828c0133c5ca01249bf1c1de98df5ec64
martinsmandina <martinsmandina@gmail.com>
2020-11-02 10:11:39 +0300

    add migration

--- app/models.py
+++ app/models.py
@@ -1,10 +1,5 @@
 from . import db
 
-@login_manager.user_loader
-def load_user(user_id):
-
-    return User.query.get(int(user_id))
-
 
 class User(db.Model):
     __tablename__ = 'users'
--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
 from app import create_app,db
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 from app.models import User
 
 # Creating app instance
 app = create_app('development')
-
 manager = Manager(app)
-
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 manager.add_command('server',Server)
 
 @manager.shell
