96010fb71574268276f85724fa874ec7bfe4142d
martinsmandina <martinsmandina@gmail.com>
2020-11-02 11:54:06 +0300

    Create class

--- app/models.py
+++ app/models.py
@@ -1,10 +1,25 @@
 from . import db
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+
+    """
+    @login_manager.user_loader Passes in a user_id to this function
+    Function queries the database and gets a user's id as a response
+    """
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
+
+    blogs = db.relationship('Blog',backref='author',lazy='dynamic')
+
 
     # securing passwords
     @property
@@ -21,5 +36,28 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer,primary_key=True)
+    blog_title = db.Column(db.String(255), nullable = False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(15), nullable=False, default='MartinJohn')
+
+    user_id = db.Column(db.Integer,db.ForeignKey ('users.id'),nullable=False)
+    comment = db.relationship('Comments', backref='blog', lazy='dynamic')
+
+    def __repr__(self):
+        return f'Title {self.Blog_title}'
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(5000))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    blog_id = db.Column(db.Integer, db.ForeignKey("blogs.id"))
+
+
+
 
    
