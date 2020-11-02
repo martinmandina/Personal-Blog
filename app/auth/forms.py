0814c13e47b9c19e725a2800bcc79bbf62d5800a
martinsmandina <martinsmandina@gmail.com>
2020-11-02 16:51:41 +0300

    add subscription form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,ValidationError
 from wtforms.validators import Required,Email,EqualTo
-from ..models import User,BlogPost,Comment
+from ..models import User
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
@@ -24,3 +24,17 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')            
     
+class SubscriptionForm(FlaskForm):
+    name = StringField('Enter your username', validators=[Required()])
+    user_email = StringField('Your Email Address', validators=[Required(), Email()])
+    submit = SubmitField('Subscribe')
+
+def validate_name(self, data_field):
+    if User.query.filter_by(username=data_field.data).first():
+        raise ValidationError('That username is already taken')
+
+def validate_user_email(self, data_field):
+    if User.query.filter_by(email=data_field.data).first():
+        raise ValidationError('There is an account with that email')
+
+        
