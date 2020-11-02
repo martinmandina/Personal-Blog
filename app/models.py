932bbfc4437695d0653828926279727bc1a387e4
martinsmandina <martinsmandina@gmail.com>
2020-11-02 10:25:22 +0300

    add auth blueprint

--- app/models.py
+++ app/models.py
@@ -1,10 +1,23 @@
 from . import db
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
+
+    # securing passwords
+    @property
+    def password(self):
+        raise AttributeError('You can not read the password Attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
 
     def __repr__(self):
         return f'User {self.username}'
