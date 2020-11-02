4a58395202225f5f3562a97f88944ca7f0341b89
martinsmandina <martinsmandina@gmail.com>
2020-11-02 14:17:52 +0300

    register header

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,22 @@
 from flask import render_template,redirect,url_for
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
 from . import auth
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Blog: login here"
+    return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
@@ -17,3 +31,4 @@ def register():
         return redirect(url_for('auth.login'))
         title = "Blog: Create New Account"
     return render_template('auth/register.html',registration_form = form)
+
