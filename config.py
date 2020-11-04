9b40990f6844788413d184cd5cde087eec8d30b5
martinsmandina <martinsmandina@gmail.com>
2020-11-04 22:24:47 +0300

    deploy

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martinmandina:alicewambui@localhost/blogy'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
@@ -15,29 +15,24 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SENDER_EMAIL = 'mandinasila@gmail.com'
 
-    @staticmethod
-    def init_app(app):
-        pass
+    # @staticmethod
+    # def init_app(app):
+    #     pass
 
 class ProdConfig(Config):
-        pass
+    pass
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
+class DevConfig(Config):
+    DEBUG = True
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martinmandina:alicewambui@localhost/blogy'
-
-
-class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martinmandina:alicewambui@localhost/blogy'
-
-    DEBUG = True
 
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
 'test': TestConfig
-
 }
 
 
